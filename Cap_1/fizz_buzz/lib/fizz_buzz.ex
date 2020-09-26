491cf365b25359d5a3f6a8e224aae2d79817b5d3
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-09-26 17:13:19 +0000

    Tratando os eventos de sucesso e erro

--- Cap_1/fizz_buzz/lib/fizz_buzz.ex
+++ Cap_1/fizz_buzz/lib/fizz_buzz.ex
@@ -6,7 +6,12 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error on file: #{reason}"
 
 end
