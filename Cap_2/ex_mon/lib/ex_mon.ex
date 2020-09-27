1c4643734c741e43592bc9148c0c5b13412a1da4
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-09-27 16:38:34 +0000

    Criando Game Agent

--- Cap_2/ex_mon/lib/ex_mon.ex
+++ Cap_2/ex_mon/lib/ex_mon.ex
@@ -1,8 +1,16 @@
 defmodule ExMon do
 
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+
+  @computer_name "Robotinik"
 
   def create_player(move_avg, move_heal, move_rnd, name) do
     Player.build(move_avg, move_heal, move_rnd, name)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
