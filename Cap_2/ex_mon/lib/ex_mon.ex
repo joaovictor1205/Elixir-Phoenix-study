f87df92f0361a83c62281626ea7ee1b270ce0d6b
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-09-27 17:03:23 +0000

    Imprimindo o estado do jogo

--- Cap_2/ex_mon/lib/ex_mon.ex
+++ Cap_2/ex_mon/lib/ex_mon.ex
@@ -1,6 +1,7 @@
 defmodule ExMon do
 
   alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
 
   @computer_name "Robotinik"
 
@@ -12,5 +13,6 @@ defmodule ExMon do
     @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
+    Status.print_round_message()
   end
 end
