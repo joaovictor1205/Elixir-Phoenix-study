6a9fbc686de6f7f22ab149517d122d18db1c13e1
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-09-28 23:12:31 +0000

    Criando ação de cura

--- Cap_2/ex_mon/lib/game/status.ex
+++ Cap_2/ex_mon/lib/game/status.ex
@@ -30,4 +30,8 @@ defmodule ExMon.Game.Status do
     IO.puts("\n ===== The Computer has attacked Player dealing #{damage} damage ===== \n")
   end
 
+  def print_move_message(player, :heal, life) do
+    IO.puts("\n ===== The #{player} has healled itself to #{life} life points ===== \n")
+  end
+
 end
