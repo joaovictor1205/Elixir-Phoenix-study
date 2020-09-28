a974c19d60b5a5cdf3f48d08eb75808fe83868da
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-09-28 21:22:02 +0000

    Imprimindo mensagem de ataque

--- Cap_2/ex_mon/lib/game/status.ex
+++ Cap_2/ex_mon/lib/game/status.ex
@@ -10,4 +10,13 @@ defmodule ExMon.Game.Status do
   def print_wrong_move_message(move) do
     IO.puts("\n ===== Invalid move: #{move} ===== \n")
   end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n ===== The Player has attacked Computer dealing #{damage} damage ===== \n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n ===== The Computer has attacked Player dealing #{damage} damage ===== \n")
+  end
+
 end
