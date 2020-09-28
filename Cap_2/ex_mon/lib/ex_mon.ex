90a5a41a8d5e587a2478b68535e21a1083ca773e
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-09-28 22:39:07 +0000

    Alterando o estado do jogo

--- Cap_2/ex_mon/lib/ex_mon.ex
+++ Cap_2/ex_mon/lib/ex_mon.ex
@@ -14,7 +14,7 @@ defmodule ExMon do
     @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
-    Status.print_round_message()
+    Status.print_round_message(Game.info())
   end
 
   def make_move(move) do
@@ -30,5 +30,7 @@ defmodule ExMon do
       :move_heal -> "Cura realizada!"
       move -> Actions.attack(move)
     end
+
+    Status.print_round_message(Game.info())
   end
 end
