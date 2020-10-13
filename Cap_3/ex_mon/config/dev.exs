257988e85fd0d6433df94e3a43481fa3a3f7478d
João Victor <joaovictor.lol12@gmail.com>
2020-10-13 19:40:16 -0300

    Criando o Trainer Pokemon Controller

--- Cap_3/ex_mon/config/dev.exs
+++ Cap_3/ex_mon/config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :ex_mon, ExMon.Repo,
   username: "postgres",
   password: "postgres",
-  database: "phoenixAPI",
+  database: "elixirphoenixcourse",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
